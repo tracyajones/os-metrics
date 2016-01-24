d34b5a878beb94903198c94dabb0df77903375fd
Tracy Jones <tjones@vmware.com>
2016-01-24 15:33:22 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='os-metrics',
+        version='1.0',
+        packages=['flask', 'urllib2', 'json'],
+        url='',
+        license='',
+        author='tracyjones',
+        author_email='tjones@vmware.com',
+        description='gather metrics'
+)
